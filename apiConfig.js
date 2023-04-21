417f1ad6525f2610a786de96760f17f163982a80
Ian Flanagan <ianianf@yahoo.comi>
2023-04-21 14:34:56 -0500

    added node.js script to use testim API

---
+++ apiConfig.js
@@ -0,0 +1,24 @@
+let config = {
+
+  myAPIKey : 'PAK-xBgEeBGOLUaSI9-KS1/Gm+OS26Svc9+y2x2BC3syVImoDYMa9LxrsSzh+Q1hVop4Z/8VxOvz6UU+riKvK',
+  url : 'https://api.testim.io/',
+  suitesURl  : 'suites/run',
+  testbyID : 'tests/run/',
+  testPlanurl : 'test-plans/run/',
+  labelURL: 'labels/run/',
+  runTestbyLabelID : 'https://api.testim.io/labels/run/',
+  runTestPlanbyID : 'https://api.testim.io/test-plans/run/',
+  branch : 'master',
+  grid : 'TESTIM-GRID',
+  parallel : '1',
+  retries : '0',
+  timeout : 600,
+  turboMode: true,
+  mySuiteID: 'yBDyWXOxwzbNkzRm',
+  myTestID: 'Gntn7GdluocTtWyT',
+  myTestPlanID: 'Z0Nmu5RJUAgk8pyO',
+  myLabelID: 'qTest'
+
+};
+
+module.exports = config;
---
+++ testimApi.js
@@ -0,0 +1,224 @@
+
+/*
+
+Author: Ian Flanagan Tricentis Devops BU 2023
+edit the apiConfig.js file to add your API token 
+and testID, suiteID, and testPlanID and or lablels
+*/
+
+let config = require('./apiConfig.js');
+
+const mysuiteID = 'yBDyWXOxwzbNkzRm';
+const mytestID = 'Gntn7GdluocTtWyT';
+const testURL = `${config.url}/tests/run/${config.mytestID}`;
+const labelURL = `https://api.testim.io/labels/run/${config.myLabelID}`;
+
+
+//const url = `${config.url}/suites/run/${mysuiteID}`;
+
+//const url = `${config.url}test-plans/run/${config.myTestPlanID}`;
+const url = labelURL;
+config.myTestPlanID
+
+let token = config.myAPIKey;
+
+function runTestbyID(testID) {
+
+
+const data = {
+
+  // Data to be sent in the request body
+
+  "branch": config.branch,
+  "grid" : config.grid,
+  "parallel": config.parallel,
+  "retries": config.retries,
+  "timeout": config.timeout,
+  "turboMode": config.turbomode,
+  "resultLabels": [
+    "Smoke Suite"
+  ]
+
+};
+
+fetch(url, {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+    "Authorization": `Bearer ${token}`
+  },
+  body: JSON.stringify(data)
+})
+.then(response => {
+  if (!response.ok) {
+    throw new Error("Network response was not ok");
+  }
+  return response.json();
+})
+.then(data => {
+  console.log(data);
+})
+.catch(error => {
+  console.error("There was an error:", error);
+});
+
+}
+
+function runSuiteByID(mysuiteID) {
+
+const data = {
+
+  "branch": config.branch,
+  "grid" : config.grid,
+  "parallel": config.parallel,
+  "retries": config.retries,
+  "timeout": config.timeout,
+  "turboMode": config.turbomode,
+  "resultLabels": [
+    "Smoke Suite"
+  ]
+
+};
+
+fetch(url, {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+    "Authorization": `Bearer ${token}`
+  },
+  body: JSON.stringify(data)
+})
+.then(response => {
+  if (!response.ok) {
+    throw new Error("Network response was not ok");
+  }
+  return response.json();
+})
+.then(data => {
+  console.log(data);
+})
+.catch(error => {
+  console.error("There was an error:", error);
+});
+
+
+}
+
+
+function runByLabelID(myLabelID) {
+
+const data = {
+
+  "branch": config.branch,
+  "grid" : config.grid,
+  "parallel": config.parallel,
+  "retries": config.retries,
+  "timeout": config.timeout,
+  "turboMode": config.turbomode,
+  "resultLabels": [
+    "Smoke Suite"
+  ]
+
+};
+
+fetch(url, {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+    "Authorization": `Bearer ${token}`
+  },
+  body: JSON.stringify(data)
+})
+.then(response => {
+  if (!response.ok) {
+    throw new Error("Network response was not ok");
+  }
+  return response.json();
+})
+.then(data => {
+  console.log(data);
+})
+.catch(error => {
+  console.error("There was an error:", error);
+});
+
+
+}
+
+
+function runTestPlanID(myTestPlanID) {
+
+let url = `https://api.testim.io/test-plans/run/${myTestPlanID}`;
+
+const data = {
+
+  "branch": config.branch,
+  "grid" : config.grid,
+  "parallel": config.parallel,
+  "retries": config.retries,
+  "timeout": config.timeout,
+  "turboMode": config.turbomode,
+  "resultLabels": [
+    "Smoke Suite"
+  ],
+   "mode": "selenium"
+
+};
+
+fetch(url, {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+    "Authorization": `Bearer ${token}`
+  },
+  body: JSON.stringify(data)
+})
+.then(response => {
+  if (!response.ok) {
+    throw new Error("Network response was not ok");
+  }
+  return response.json();
+})
+.then(data => {
+  console.log(data);
+})
+.catch(error => {
+  console.error("There was an error:", error);
+});
+
+
+}
+
+
+function getTests (projectID) {
+
+  let url = 'https://api.testim.io/tests';
+
+ fetch(url, {method: 'GET', headers: {
+         'Accept': 'application/json',
+         'Authorization': `Bearer ${token}`
+     },
+     timeout: 5000
+ }).then(response => response.json())
+ 
+ .then(function(data) {
+ 
+     console.log('In Data portion of promise');
+ 
+     // define a new array to add the testim test case IDS to for the next step
+     let myTests = [];
+      Object.values(data)[0].forEach(test => myTests.push(test._id));    
+      // console.log('My Tests is  =' +myTests);
+
+      console.log('tests old way print out');
+
+      // https://app.testim.io/#/project/yUHggaUM53fuXM0PejOu/branch/master/test/ToPNnwbAIwm0xB1k
+      myTests.forEach(testID => console.log(`https://app.testim.io/#/project/${projectID}/branch/master/automate/test/${testID}`));
+ 
+  });
+  
+ };
+
+getTests('yUHggaUM53fuXM0PejOu');
+//runTestPlanID(config.myTestPlanID);
+
